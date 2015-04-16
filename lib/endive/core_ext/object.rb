716e5af2e7c49774412cc93693cc2928a36cdee8
Danil Nurgaliev <jkonalegi@gmail.com>
2015-04-16 17:40:29 +0300

    add try method for objects core_ext

--- lib/endive/core_ext/object.rb
+++ lib/endive/core_ext/object.rb
@@ -6,4 +6,13 @@ class Object
   def present?
     !blank?
   end
+
+  def try(*a, &b)
+    if a.empty? && block_given?
+      yield self
+    else
+      public_send(*a, &b) if respond_to?(a.first)
+    end
+  end
+
 end
