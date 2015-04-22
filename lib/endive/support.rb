79a33ebc31e7ae470f6282679d928e2dc61d834a
Danil Nurgaliev <jkonalegi@gmail.com>
2015-04-22 21:22:54 +0300

    slightly refactored param paraser

--- lib/endive/support.rb
+++ lib/endive/support.rb
@@ -1,4 +1,6 @@
 require 'endive/support/sym_hash'
+require 'endive/support/string_inquirer'
+require 'endive/support/param_parser'
 
 module Endive
   module Support
---
+++ lib/endive/support/param_parser.rb
@@ -0,0 +1,45 @@
+require 'cgi'
+module Endive
+  module Support
+    class ParamsParser
+      class << self
+
+        def get_params method, headers, query_string, body
+          case method
+          when :get, :detete, :options
+            parse_request query_string
+          when :post, :put
+            parse_request query_string, headers, body
+          end
+        end
+
+        def parse_request query_string, headers = nil, body = nil
+          complete_request_hash = parse_formencoded(query_string || '')
+          return complete_request_hash unless body
+          complete_request_hash.merge(parse_post_body(body, headers[CONTENT_TYPE_HEADER_KEY]))
+        end
+
+        def parse_post_body body, content_type_header
+          content_types = content_type_header ? content_type_header.split(';') : []
+          body = body.to_s
+
+          return {} if body.blank?
+
+          return parse_formencoded(body) if content_types.include?(FORM_TYPE)
+          return SymHash.new(JSON.parse(body)) if content_types.include?(JSON_TYPE)
+
+          {}
+        end
+
+        def parse_formencoded str
+          str.split('&').reduce(Endive::Support::SymHash.new) do |p, kv|
+            key, value = kv.split('=').map {|s| CGI.unescape s}
+            p[key] = value
+            p
+          end
+        end
+
+      end
+    end
+  end
+end
