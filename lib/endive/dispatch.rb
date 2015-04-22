3e4fae2d2468aee2545d8d31bbf662fdeb5d93c0
Danil Nurgaliev <jkonalegi@gmail.com>
2015-04-22 21:25:19 +0300

    remove param parser and connection handler from dispatch module

--- lib/endive/dispatch.rb
+++ lib/endive/dispatch.rb
@@ -1,6 +1,4 @@
 require 'endive/dispatch/base_controller'
-require 'endive/dispatch/connection_handler'
-require 'endive/dispatch/params_parser'
 require 'endive/dispatch/responder'
 
 module Endive
