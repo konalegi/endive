a7527870833001b37843ae95ec0a4e1ec812187f
Danil Nurgaliev <jkonalegi@gmail.com>
2015-05-13 17:11:20 +0300

    refactor profiler

--- lib/endive/support/profiler.rb
+++ lib/endive/support/profiler.rb
@@ -1,32 +1,39 @@
+require 'ruby-prof'
 module Endive
   module Support
     class Profiler
-      require 'ruby-prof'
+
       class << self
-        def profile_simple &block
-          result = RubyProf.profile do
-            yield
-          end
 
-          printer = RubyProf::FlatPrinter.new(result)
-          printer.print(STDOUT)
+        def profile_simple filename = nil, &block
+          print_with_printer(RubyProf::FlatPrinter, filename, block)
         end
 
-        def profile_with_graph_printer &block
-          result = RubyProf.profile do
-            yield
-          end
-
-          printer = RubyProf::GraphPrinter.new(result)
-          printer.print(STDOUT)
+        def profile_with_graph_printer filename = nil, &block
+          print_with_printer(RubyProf::GraphPrinter, filename, block)
         end
 
-        def execution_time &block
+        def execution_time msg, &block
           now = Time.now.to_f
           yield
-          ((Time.now.to_f - now.to_f) * 1000).round(2)
+          Endive.logger.debug msg % [((Time.now.to_f - now.to_f) * 1000).round(2)]
         end
 
+        private
+          def standart_filename
+            "#{Endive.root}/tmp/profile_#{Time.now.to_i}.log"
+          end
+
+          def print_with_printer(printer_class, filename, block)
+            File.open(filename || standart_filename, 'w') do |file|
+              result = RubyProf.profile do
+                block.call
+              end
+
+              printer = printer_class.new(result)
+              printer.print(file)
+            end
+          end
       end
     end
   end
