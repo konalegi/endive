ff985c6cf72fd66396f3dda78636403c708ff2ce
Insaf Gallyamov <0nsaf@mail.ru>
2015-06-08 10:18:55 +0300

    controllers testing update

--- lib/endive/testing/controller_test_base.rb
+++ lib/endive/testing/controller_test_base.rb
@@ -2,12 +2,18 @@ module Endive
   module Testing
     class ControllerTestBase < Minitest::Test
       attr_reader :data, :headers, :status
+      attr_accessor :request_headers
+
+      def initialize(param)
+        super
+        @request_headers = {}
+      end
 
       [:get, :post, :put, :delete].each do |meth|
         define_method meth do |path, params = {}|
           found_route = Endive.application.config.router.find_route(meth, path)
           found_route[:options].merge!(params)
-          responder = Dispatch::Responder.new(found_route[:controller], found_route[:action])
+          responder = Dispatch::Responder.new(found_route[:controller], found_route[:action], request_headers)
           @status, @data, @headers = responder.dispatch(found_route)
         end
       end
