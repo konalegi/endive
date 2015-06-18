74ba6d66874003f8b3e907755d6948d5a8151e17
Insaf Gallyamov <0nsaf@mail.ru>
2015-06-18 11:04:47 +0300

    controller tests update

--- lib/endive/testing/controller_test_base.rb
+++ lib/endive/testing/controller_test_base.rb
@@ -1,25 +1,40 @@
 module Endive
   module Testing
     class ControllerTestBase < Minitest::Test
-      attr_reader :data, :headers, :status
-      attr_accessor :request_headers
+      attr_reader :data, :headers, :status, :request
 
-      def initialize(param)
+      def initialize(params)
         super
-        @request_headers = {}
+        @request = Request.new
       end
 
       [:get, :post, :put, :delete].each do |meth|
         define_method meth do |path, params = {}|
           found_route = Endive.application.config.router.find_route(meth, path)
           found_route[:options].merge!(params)
-          responder = Dispatch::Responder.new(found_route[:controller], found_route[:action], meth, request_headers)
+          request.method = meth
+          responder = Dispatch::Responder.new(found_route[:controller], found_route[:action], request)
           @status, @data, @headers = responder.dispatch(found_route)
         end
       end
 
       def assert_response(status)
         assert_equal status, @status
+      end
+
+    end
+
+
+    class Request
+      attr_accessor :headers, :method
+
+      def initialize
+        @headers = {}
+        @method = nil
+      end
+
+      def websocket?
+        method == 'websocket'
       end
 
     end
